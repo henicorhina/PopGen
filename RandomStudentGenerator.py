6ed3fa67b6bc60683cbed8a48366bc9b8ac41a8d
Oscar Johnson <ojohns7@tigers.lsu.edu>
2015-04-22 20:33:44 -0500

    Create RandomStudentGenerator.py

---
+++ RandomStudentGenerator.py
@@ -0,0 +1,7 @@
+#! /usr/bin/env python
+import random
+
+list = ["bill", "rafa", "rob", "zach", "subir", "david", "oscar", "genevieve", "cody", "jasmine", "vallmer", "kristin", "mark", "joanna", "brandon", "viv", "duhon", "ryan"]
+
+for x in range (1):
+	print random.choice(list)
